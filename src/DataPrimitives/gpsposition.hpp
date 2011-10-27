ccb8b333212571ef88a14701949731157f24ca6e
Lena Brueder <Lena.Brueder@rub.de>
2011-10-27 17:18:49 +0200

    Doxygen-Kommentare in gpsposition.hpp hinzugefügt, kleinere Änderungen im Code.

--- src/DataPrimitives/gpsposition.hpp
+++ src/DataPrimitives/gpsposition.hpp
@@ -1,36 +1,65 @@
-//      This program is free software; you can redistribute it and/or modify
-//      it under the terms of the GNU General Public License as published by
-//      the Free Software Foundation; either version 2 of the License, or
-//      (at your option) any later version.
-//      
-//      This program is distributed in the hope that it will be useful,
-//      but WITHOUT ANY WARRANTY; without even the implied warranty of
-//      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//      GNU General Public License for more details.
-//      
-//      You should have received a copy of the GNU General Public License
-//      along with this program; if not, write to the Free Software
-//      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//      MA 02110-1301, USA.
-//      
-
-
 #ifndef GPSPOSITION_HPP
 #define GPSPOSITION_HPP
 
-#include "DataPrimitiveDefines.hpp"
 #include "math.h"
+
+/**
+ * @brief GPSPosition speichert Längen- und Breitengrad und ist entsprechend eine Repräsentation eines
+ * Punktes auf der Erdoberfläche.
+ * 
+ * Höhendaten werden nicht gespeichert. Es ist möglich, auf die Eigenschaften statt in Grad, auch
+ * in Bogenmaß zuzugreifen - lediglich der Konstruktor ist nur für die (gebräuchlichere) Angabe in
+ * Grad verfügbar.
+ * 
+ * @author Lena Brüder
+ * 
+ * @copyright Lizenz: GNU GPL v3
+ */
+
+#define EARTH_RADIUS 6371000
 
 class GPSPosition
 {
 public:
+    /**
+     * Gibt den Längengrad des Punktes zurück, in Grad.
+     * @return den Längengrad in Grad
+     */
     virtual double getLon() const;
+    /**
+     * Gibt den Breitengrad des Punktes zurück, in Grad.
+     * @return den Breitengrad in Grad
+     */
     virtual double getLat() const;
+    /**
+     * Gibt den Längengrad des Punktes zurück, in Bogenmaß.
+     * @return den Längengrad in Bogenmaß
+     */
     virtual double getRadLon() const;
+    /**
+     * Gibt den Breitengrad des Punktes zurück, in Bogenmaß.
+     * @return den Breitengrad in Bogenmaß
+     */
     virtual double getRadLat() const;
+    /**
+     * Setzt den Längengrad des Punktes in Grad.
+     * @param lon Längengrad in Grad
+     */
     virtual void setLon(double lon);
+    /**
+     * Setzt den Breitengrad des Punktes in Grad.
+     * @param lat Breitengrad in Grad
+     */
     virtual void setLat(double lat);
+    /**
+     * Setzt den Längengrad des Punktes in Bogenmaß.
+     * @param lon Längengrad in Bogenmaß
+     */
     virtual void setRadLon(double lon);
+    /**
+     * Setzt den Breitengrad des Punktes in Bogenmaß.
+     * @param lat Breitengrad in Bogenmaß
+     */
     virtual void setRadLat(double lat);
 
     //Die folgenden Funktionen dienen zur Berechnung von Zusammenhängen mehrerer Koordinaten (Entfernung, ...)
@@ -38,36 +67,63 @@ public:
      * Berechnet die Entfernung zweier Punkte in Metern.
      * Die Berechnung erfolgt nicht nach dem WGS84-Ellipsoid, kann daher
      * für größere Entfernungen ungenau werden.
+     * @param p2 ein anderer Punkt
+     * @return die Entfernung zwischen diesem und dem anderen Punkt.
      */
     virtual double calcDistance(GPSPosition p2) const;
     /**
-     * Berechnet den Kurswinkel, gesehen vom Punkt auf p2 zu, in Grad.
+     * Berechnet den Kurswinkel (gegenüber Nord), gesehen vom Punkt auf p2 zu, in Grad.
+     * @param p2 ein anderer Punkt
+     * @return den Kurswinkel, gesehen vom Punkt auf p2 zu, in Grad
      */
     virtual double calcCourseAngle(GPSPosition p2) const;
 
     /**
      * Wegpunktprojektion.
-     * Berechnet einen Punkt in einer Entfernung von distance Metern unter einem Kurswinkel
-     * von courseAngle.
-     * @param courseAngle
-     * @param distance
+     * Berechnet einen Punkt in einer Entfernung von <code>distance</code> Metern unter einem Kurswinkel
+     * von <code>courseAngle</code>.
+     * @param courseAngle der Kurswinkel zu Nord
+     * @param distance Die Entfernung vom Startpunkt
      * @return
      */
     virtual GPSPosition calcPositionInDistance(double courseAngle, double distance) const;
+    
+    /**
+     * Gibt zurück, ob die Position noch auf (0.0/0.0) gesetzt ist.
+     * @return <code>true</code>, wenn <code>lon==lat==0.0</code>, <code>false</code> sonst.
+     */
     virtual bool isInitialized() const;
-
+    
+    /**
+     * Initialisiert eine GPSPosition mit den Werten <code>lon=0.0, lat=0.0</code>
+     */
     GPSPosition() : lon(0.0), lat(0.0) {}
+    /**
+     * Initialisiert eine GPSPosition mit den angegebenen Werten.
+     * @param lon der Längengrad
+     * @param lat der Breitengrad
+     */
     GPSPosition(double lon, double lat) : lon(lon), lat(lat) {}
     virtual ~GPSPosition() {}
 protected:
+    /**
+     * Längengrad
+     */
     double lon;
+    
+    /**
+     * Breitengrad
+     */
     double lat;
 
 private:
+    
     double calcXi(GPSPosition p2) const;
     /**
      * Rechnet um von Grad nach Bogenmaß. Template-Funktion,
      * gedacht für float und double.
+     * @param deg Winkel in Grad
+     * @return Winkel in Bogenmaß
      */
     template<typename T>
     T deg2rad(T deg) const
@@ -78,6 +134,8 @@ private:
     /**
      * Rechnet um von Bogenmaß nach Grad. Template-Funktion,
      * gedacht für float und double.
+     * @param rad Winkel in Bogenmaß
+     * @return Winkel in Grad
      */
     template<typename T>
     T rad2deg(T rad) const
