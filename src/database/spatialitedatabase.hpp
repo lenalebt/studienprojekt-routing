38a38c16fcabf45ba4372a1845775b6737c21493
Lena brüder (eee) <lena_lebt@gmx.de>
2011-11-24 19:02:22 +0100

    Spatialite-Dateien hinzugefügt, kein Inhalt

---
+++ src/database/spatialitedatabase.hpp
@@ -0,0 +1,26 @@
+#ifndef SPATIALITE_HPP
+#define SPATIALITE_HPP
+
+#include "database.hpp"
+
+class SpatialiteDatabaseConnection : public DatabaseConnection
+{
+private:
+    
+public:
+    void close();
+    void open(QString dbConnectionString);
+    bool isDBOpen();
+    QVector<boost::shared_ptr<RoutingNode> > getNodes(const GPSPosition& searchMidpoint, double radius);
+    QVector<boost::shared_ptr<RoutingNode> > getNodes(const GPSPosition& ulCorner, const GPSPosition& brCorner);
+    void saveNode(const RoutingNode& node);
+    QVector<boost::shared_ptr<RoutingEdge> > getEdgesByStartNodeID(boost::uint64_t startNodeID);
+    QVector<boost::shared_ptr<RoutingEdge> > getEdgesByEndNodeID(boost::uint64_t endNodeID);
+    boost::shared_ptr<RoutingEdge> getEdgesByEdgeID(boost::uint64_t edgeID);
+    void saveEdge(const RoutingEdge& edge);
+    void saveEdge(const RoutingEdge& edge, QString name);
+    QString getStreetName(const RoutingEdge& edge);
+
+};
+
+#endif //SPATIALITE_HPP
