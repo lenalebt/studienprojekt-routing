96682a9a1daed8c1c37c7209018501a9b860f126
Lena Brueder <Lena.Brueder@rub.de>
2011-12-02 10:26:26 +0100

    Es fehlten in OSMProperty Implemetierungen von ein paar Funktionen und Operatoren.
    Test zu OSMNode erweitert.

--- src/dataprimitives/osmproperty.cpp
+++ src/dataprimitives/osmproperty.cpp
@@ -1,4 +1,29 @@
 #include "osmproperty.hpp"
+#include "tests.hpp"
+
+std::ostream& operator<<(std::ostream& os, const OSMProperty& prop)
+{
+    os << "key: \"" << prop.getKey() << "\" value: \"" << prop.getValue() << "\"";
+    return os;
+}
+
+std::size_t hash_value(QString const& b)
+{
+    return boost::hash_value(b.toStdString());
+}
+
+bool OSMProperty::operator==(const OSMProperty& prop) const
+{
+    if (this->key == prop.key)	//key gleicht: Voraussetzung für die meisten Sachen.
+    {
+        if (this->value == prop.value || this->value.get().isEmpty() || prop.value.get().isEmpty())
+            return true;
+        else
+            return false;
+    }
+    else
+        return false;
+}
 
 namespace biker_tests
 {
