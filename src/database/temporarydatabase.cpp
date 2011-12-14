59075b9474fea57d5d90bfbd7fe49c074bc4ce53
Lena brüder (eee) <lena_lebt@gmx.de>
2011-12-14 15:58:51 +0100

    Dateien für temporäre OSM-Datenbank hinzugefügt

---
+++ src/database/temporarydatabase.cpp
@@ -0,0 +1,1 @@
+#include "temporarydatabase.hpp"
---
+++ src/database/temporarydatabase.hpp
@@ -0,0 +1,12 @@
+#ifndef TEMPORARYDATABASE_HPP
+#define TEMPORARYDATABASE_HPP
+
+class TemporaryOSMDatabase
+{
+private:
+    
+public:
+    
+}
+
+#endif //TEMPORARYDATABASE_HPP
