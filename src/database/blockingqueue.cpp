656e495f106f2a4ed64b2f9501e3cbffa7c19539
Lena Brueder <Lena.Brueder@rub.de>
2011-12-21 10:49:57 +0100

    Anfang von BlockingQueue implementiert

--- src/database/blockingqueue.cpp
+++ src/database/blockingqueue.cpp
@@ -3,25 +3,61 @@
 template <typename T> 
 bool BlockingQueue<T>::dequeue(T& t)
 {
+    QMutexLocker locker(&_mutex);
+    
+    if (isEmpty())
+    {
+        bool waitForElement=true;
+        while (waitForElement)
+        {
+            notEmpty.wait(&_mutex);
+            if (isEmpty())
+            {
+                if (!_queueDestroyed)
+                {
+                    
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                waitForElement = false;
+            }
+        }
+    }
+    else
+    {
+        t = _queue.dequeue();
+        return true;
+    }
     return false;
 }
 
 template <typename T> 
 bool BlockingQueue<T>::enqueue(const T &t)
 {
+    QMutexLocker locker(&_mutex);
     return false;
 }
 
 template <typename T> 
 bool BlockingQueue<T>::isEmpty()
 {
-    return true;
+    QMutexLocker locker(&_mutex);
+    
+    return _queue.isEmpty();
 }
 
 template <typename T> 
 void BlockingQueue<T>::destroyQueue()
 {
-    
+    QMutexLocker locker(&_mutex);
+    _queueDestroyed = true;
+    notFull.wakeAll();
+    notEmpty.wakeAll();
 }
 
 
@@ -29,6 +65,7 @@ namespace biker_tests
 {
     int testBlockingQueue()
     {
+        BlockingQueue<int> bQueue(10);
         return EXIT_FAILURE;
     }
 }
--- src/database/blockingqueue.hpp
+++ src/database/blockingqueue.hpp
@@ -3,13 +3,20 @@
 
 #include <QMutex>
 #include <QQueue>
+#include <QWaitCondition>
 #include "tests.hpp"
 
 /**
  * @brief Implementierung einer blockierenden, threadsicheren Queue mit
  *      beschränkter Kapazität
  * 
- * 
+ * Die Queue blockiert, wenn sie voll ist und ein Element hinzugefügt werden
+ * soll - ebenso, wenn sie leer ist und eines entfernt werden soll. Wenn
+ * die Queue in einem Zustand ist, in dem keine weiteren Elemente mehr
+ * hinzugefügt werden sollen, wird sie zerstört (->destroyQueue()). Mit Aufruf
+ * dieser Funktion werden alle eventuell auf neue Elemente wartenden Threads
+ * geweckt. Die Queue kann noch geleert werden, so sie noch Elemente enthält -
+ * es können aber keine Elemente mehr hinzugefügt werden.
  * 
  * @author Lena Brüder
  * @date 2011-11-07
@@ -24,19 +31,28 @@ class BlockingQueue
 private:
     int _size;
     QQueue<T> _queue;
+    QMutex _mutex;
+    QWaitCondition notFull;
+    QWaitCondition notEmpty;
+    bool _queueDestroyed;
 public:
     /**
      * @brief Initialisiert die Queue mit der Kapazität <code>size</code>
      * @param size Bestimmt die Kapazität der Queue, in Anzahl Elementen.
      */
-    BlockingQueue(int size) : _size(size) {}
+    BlockingQueue(int size) : _size(size), _queue(QQueue<T>()),
+        _mutex(), notFull(), notEmpty(), _queueDestroyed(false)
+    {
+        
+    }
     
     /**
      * @brief Entfernt ein Element aus der Queue.
      * 
      * Blockiert, wenn kein Element vorhanden ist. Gibt <code>false</code>
-     * zurück, wenn die Queue zerstört wurde und daher
-     * kein Element zurückgegeben werden kann, und auch nie eines folgen wird.
+     * zurück, wenn die Queue zerstört wurde und kein Element mehr vorhanden
+     * ist - in diesem Fall kann nie mehr
+     * ein Element zurückgegeben werden.
      * 
      * @param[out] t In dieser Variable wird das Element abgelegt, das
      *      aus der Queue entfernt wurde, so es eines gab.
@@ -69,13 +85,11 @@ public:
      * @remarks Diese Funktion ist bei Zugriffen auf Threads evtl. wertlos.
      * @return Ob die Queue aktuell leer ist.
      * @see destroyQueue()
-     * @todo Implementierung fehlt
      */
     bool isEmpty();
     /**
      * @brief Zerstört die Queue und sorgt dafür, dass alle
      *  blockierenden Zugriffe abgebrochen werden.
-     * @todo Implementierung fehlt
      */
     void destroyQueue();
 };
