6f68d568fe5fdf795075e632a8fa07d22c6ffdc7
[sebastian] <[sebastian.koehlert@ruhr-uni-bochum.de]>
2011-12-21 22:01:52 +0100

    Preprocessing erstellt, angefangen, Funktionen in der .hpp zu definieren.

---
+++ src/preprocessing/datapreprocessing.hpp
@@ -0,0 +1,42 @@
+#ifndef DATAPREPROCESSING_HPP
+#define DATAPREPROCESSING_HPP
+
+#include "database.hpp"
+#include "temporarydatabase.hpp"
+#include "blockingqueue.hpp"
+#include "routingedge.hpp"
+
+/**
+ * @brief Diese Klasse kuemmert sich um jegliche Form der Datenvorverarbeitung
+ * 
+ * @ingroup preprocessing
+ * @author Sebastian Koehlert
+ * @date 2011-12-21
+ * @copyright GNU GPL v3
+ * @todo erstmal noch ein todo schreiben ;)
+ */
+ 
+class DataPreprocessing
+{
+	private:
+
+	public:
+	//TODO: sinnvolle Kategorien definieren
+	enum Category
+	{
+	  cat1= 0,
+	  cat2,
+	  cat3,
+	  catn
+	};
+	
+	bool feedParser();
+	
+	/**
+	* @brief Gibt die Kategorie der als Parameter übergebenen Kante aus
+	* @param edgeID Die Kante, dessen Kategorie ausgegeben werden soll.
+	* @return Kategorie der Kante.
+	*/
+	Category getCategoryEdgeID(boost::uint64_t edgeID);
+};
+#endif //DATAPREPROCESSING_HPP
