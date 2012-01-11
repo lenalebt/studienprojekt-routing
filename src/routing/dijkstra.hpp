a26373bbcbfe029d258e2760bf0ee15257d59706
Lena Brueder <Lena.Brueder@rub.de>
2012-01-11 10:09:39 +0100

    Details für Routing: Klasse MultithreadedDijkstraRouter eingefügt (ohne Inhalt)

--- src/routing/dijkstra.hpp
+++ src/routing/dijkstra.hpp
@@ -5,7 +5,8 @@
 #include "router.hpp"
 
 /**
- * @brief Implementiert den klassischen Dijkstra-Algorithmus.
+ * @brief Implementiert den klassischen Dijkstra-Algorithmus in einer
+ *      Version mit einem Thread.
  * 
  * @author Lena Brüder
  * @date 2011-12-23
@@ -14,6 +15,24 @@
  * @ingroup routing
  */
 class DijkstraRouter : public Router
+{
+private:
+    
+public:
+    
+};
+
+/**
+ * @brief Implementiert den Algorithmus von Dijkstra mit zwei Threads:
+ *      Einen vom Start-, und einen vom Zielpunkt aus.
+ * 
+ * @author Lena Brüder
+ * @date 2011-12-23
+ * @copyright GNU GPL v3
+ * @todo Implementieren!
+ * @ingroup routing
+ */
+class MultithreadedDijkstraRouter : public Router
 {
 private:
     
