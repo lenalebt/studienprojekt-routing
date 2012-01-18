54a023a8995284fcb83e8f9e8ebb73f3bdb447b2
Lena brüder (eee) <lena_lebt@gmx.de>
2012-01-18 16:54:10 +0100

    Dijkstra-Anfänge implementiert

--- src/routing/dijkstra.cpp
+++ src/routing/dijkstra.cpp
@@ -1,1 +1,89 @@
 #include "dijkstra.hpp"
+#include "tests.hpp"
+
+/**
+ * @todo: Implementieren
+ */
+GPSRoute DijkstraRouter::calculateShortestRoute(const GPSPosition& startPosition, const GPSPosition& endPosition)
+{
+    if (!_db->isDBOpen())
+    {
+        GPSRoute route;
+        return route;
+    }
+    else
+    {
+        RoutingNode startNode, endNode;
+        QVector<boost::shared_ptr<RoutingNode> > nodeList;
+        
+        nodeList = _db->getNodes(startPosition, 50.0);
+        if (nodeList.isEmpty())
+        {
+            nodeList = _db->getNodes(startPosition, 500.0);
+            if (nodeList.isEmpty())
+            {
+                nodeList = _db->getNodes(startPosition, 5000.0);
+                if (nodeList.isEmpty())
+                {
+                    //Okay, im Umkreis von 5000m nix gefunden: Dann keine Route gefunden.
+                    GPSRoute route;
+                    return route;
+                }
+            }
+        }
+        //TODO: nodeList nach nächstem Knoten durchsuchen.
+        
+        nodeList = _db->getNodes(endPosition, 50.0);
+        if (nodeList.isEmpty())
+        {
+            nodeList = _db->getNodes(endPosition, 500.0);
+            if (nodeList.isEmpty())
+            {
+                nodeList = _db->getNodes(endPosition, 5000.0);
+                if (nodeList.isEmpty())
+                {
+                    //Okay, im Umkreis von 5000m nix gefunden: Dann keine Route gefunden.
+                    GPSRoute route;
+                    return route;
+                }
+            }
+        }
+        //TODO: nodeList nach nächstem Knoten durchsuchen.
+        
+        //TODO
+        GPSRoute route;
+        return route;
+    }
+}
+
+/**
+ * @todo: Implementieren
+ */
+GPSRoute DijkstraRouter::calculateShortestRoute(const RoutingNode& startNode, const RoutingNode& endNode)
+{
+    if (!_db->isDBOpen())
+    {
+        GPSRoute route;
+        return route;
+    }
+    else
+    {
+        //TODO
+        GPSRoute route;
+        return route;
+    }
+}
+
+DijkstraRouter::DijkstraRouter(DatabaseConnection* db) :
+    _db(db)
+{
+    
+}
+
+namespace biker_tests
+{
+    int testDijkstraRouter()
+    {
+        return EXIT_FAILURE;
+    }
+}
--- src/tests.cpp
+++ src/tests.cpp
@@ -24,6 +24,7 @@
 #include "heap.hpp"
 #include "webserver.hpp"
 #include "potentialfunction.hpp"
+#include "dijkstra.hpp"
 
 //für EXIT_SUCCESS und EXIT_FAILURE
 #include <boost/program_options.hpp>
@@ -212,6 +213,8 @@ namespace biker_tests
             return biker_tests::testWebServer();
         else if (testName == "potentialfunction")
             return biker_tests::testPotentialFunction();
+        else if (testName == "dijkstrarouter")
+            return biker_tests::testDijkstraRouter();
         
         //Anpassen, falls Fehler auftraten!
         std::cerr << "error: did not find test \"" << testName << "\"." << std::endl;
