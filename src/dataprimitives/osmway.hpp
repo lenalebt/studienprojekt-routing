b74f60c03f189137657012ac742c594d9a800ec1
Lena brüder (eee) <lena_lebt@gmx.de>
2012-01-25 14:28:18 +0100

    Gui verbessert, Details am Way

--- src/dataprimitives/osmway.hpp
+++ src/dataprimitives/osmway.hpp
@@ -97,7 +97,7 @@ public:
      * @brief Erstellt aus dem Weg die zugekörigen Kanten als OSMEdge.
      * @return Die Liste aller Kanten des Weges.
      */
-    QVector<OSMEdge> getEdgeList(); //TODO implementierung
+    QVector<OSMEdge> getEdgeList();
     
     /**
      * @brief Gibt zurück, ob es sich um einen Weg mit Einbahneigenschaften handelt.
