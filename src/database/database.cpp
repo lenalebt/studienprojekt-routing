cabb138d48717d7e32bb0c0fc1fe5ffd2716b0ae
Lena brüder (eee) <lena_lebt@gmx.de>
2012-02-03 11:28:40 +0100

    Linker-Fehler beseitigt

--- src/database/database.cpp
+++ src/database/database.cpp
@@ -1,3 +1,3 @@
 #include "database.hpp"
 
-boost::shared_ptr<DatabaseConnection> _globalInstance = boost::shared_ptr<DatabaseConnection>();
+boost::shared_ptr<DatabaseConnection> DatabaseConnection::_globalInstance = boost::shared_ptr<DatabaseConnection>();
