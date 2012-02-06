69e29a7a1229d955dc938bb1b4ff89d99aa68ad7
Lena brüder (eee) <lena_lebt@gmx.de>
2012-02-06 16:01:17 +0100

    Änderungen rückgängig gemacht von Sebastians DataPreprocessing

--- src/preprocessing/datapreprocessing.cpp
+++ src/preprocessing/datapreprocessing.cpp
@@ -25,20 +25,18 @@ bool DataPreprocessing::startparser(QString fileToParse, QString dbFilename)
     tmpFile.remove();
     _tmpDBConnection.open(tmpFilename);
     
+    std::cerr << "temp file created." << std::endl;
+
     //Prueft, ob .osm oder .pbf am Ende vorhanden
     if(fileToParse.endsWith(".osm"))
     {
         _osmParser.reset(new OSMParser(&_nodeQueue, &_wayQueue, &_turnRestrictionQueue));
         QFuture<bool> future = QtConcurrent::run(_osmParser.get(), &OSMParser::parse, fileToParse);
-        
+            
         saveNodeToTmpDatabase();
-        saveEdgeToTmpDatabase();
+        saveEdgeToTmpDatabase();        
         saveTurnRestrictionToTmpDatabase();
-        
-        std::cerr << "creating indexes" << std::endl;
-        _tmpDBConnection.createIndexes();
-        _finalDBConnection->createIndexes();
-        
+                
         future.waitForFinished();
         return true;
     }
@@ -46,16 +44,13 @@ bool DataPreprocessing::startparser(QString fileToParse, QString dbFilename)
     {
         _pbfParser.reset(new PBFParser(&_nodeQueue, &_wayQueue, &_turnRestrictionQueue));
         QFuture<bool> future = QtConcurrent::run(_pbfParser.get(), &PBFParser::parse, fileToParse);
-        
+
         saveNodeToTmpDatabase();
         saveEdgeToTmpDatabase();
         saveTurnRestrictionToTmpDatabase();
-        
-        std::cerr << "creating indexes" << std::endl;
-        _tmpDBConnection.createIndexes();
-        _finalDBConnection->createIndexes();
-        
+
         future.waitForFinished();
+
         return true;
     }
     else
@@ -64,19 +59,16 @@ bool DataPreprocessing::startparser(QString fileToParse, QString dbFilename)
     }
 }
 
-/**
- * @todo Alle paar tausend/zehntausend Nodes die Transaktion schließen und wieder öffnen
- */
 void DataPreprocessing::saveNodeToTmpDatabase()
 {
     std::cerr << "Parsing Nodes..." << std::endl;
     _finalDBConnection->beginTransaction();
-    //_tmpDBConnection.beginTransaction();
-    int nodeCount=0;
+    _tmpDBConnection.beginTransaction();
     while(_nodeQueue.dequeue(_osmNode))
     {
-        if(/*_tmpDBConnection.saveOSMNode(*_osmNode) == */true)
+        if(_tmpDBConnection.saveOSMNode(*_osmNode) == true)
         {
+            std::cerr << "node saved to tmpdb" << std::endl;
         }
         else
         {
@@ -84,60 +76,44 @@ void DataPreprocessing::saveNodeToTmpDatabase()
         }
                 
         routingNode = boost::shared_ptr<RoutingNode>(new RoutingNode(_osmNode->getID(), _osmNode->getLat(), _osmNode->getLon()));
-        saveNodeToDatabase(*routingNode);
-        
-        if (++nodeCount == 100000)
-        {
-            nodeCount = 0;
-            _finalDBConnection->endTransaction();
-            //_tmpDBConnection.endTransaction();
-            
-            _finalDBConnection->beginTransaction();
-            //_tmpDBConnection.beginTransaction();
-        }
+        //saveNodeToDatabase(*routingNode);
     }
     _finalDBConnection->endTransaction();
-    //_tmpDBConnection.endTransaction();
+    _tmpDBConnection.endTransaction();
 }
 
-/**
- * @todo Alle paar tausend/zehntausend Edges die Transaktion schließen und wieder öffnen
- */
 void DataPreprocessing::saveEdgeToTmpDatabase()
 {
     std::cerr << "Parsing Ways..." << std::endl;
     _finalDBConnection->beginTransaction();
-    //_tmpDBConnection.beginTransaction();
+    _tmpDBConnection.beginTransaction();
     boost::uint64_t edgeID=0;
-    int wayCount=0;
+    //TODO: nochmal ueberlegen, ob if-Abfrage nicht sinnvoller als while-loop
     while(_wayQueue.dequeue(_osmWay))
     {
         //edges aus way extrahieren
         QVector<OSMEdge> edgeList = _osmWay->getEdgeList();
         for(int i = 0; i < edgeList.size(); i++)
         {
-            if(/*_tmpDBConnection.saveOSMEdge(edgeList[i]) == */true)
+            if(_tmpDBConnection.saveOSMEdge(edgeList[i]) == true)
             {
+                std::cerr << "edge saved" << std::endl;
             }
             else
             {
                 std::cerr << "edge NOT saved" << std::endl;
             }
-            routingEdge = boost::shared_ptr<RoutingEdge>(new RoutingEdge(edgeID++, RoutingNode::convertIDToLongFormat(edgeList[i].getStartNode()), RoutingNode::convertIDToLongFormat(edgeList[i].getEndNode())));
-            _finalDBConnection->saveEdge(*routingEdge);
-        }
-        if (++wayCount == 100000)
-        {
-            wayCount = 0;
-            _finalDBConnection->endTransaction();
-            //_tmpDBConnection.endTransaction();
+            routingEdge = boost::shared_ptr<RoutingEdge>(new RoutingEdge(edgeList[i].getID(), edgeList[i].getStartNode(), edgeList[i].getEndNode()));
             
-            _finalDBConnection->beginTransaction();
-            //_tmpDBConnection.beginTransaction();
+            //TODO: Bevor in finale Datenbank gespeichert wird, Hier die Kategorisierung starten
+            categorizeEdge(*routingEdge);
+            
+            //speichert routingEdge in die finale Datenbank
+            //_finalDBConnection->saveEdge(*routingEdge);
         }
     }
     _finalDBConnection->endTransaction();
-    //_tmpDBConnection.endTransaction();
+    _tmpDBConnection.endTransaction();
 }
 
 void DataPreprocessing::saveTurnRestrictionToTmpDatabase()
@@ -152,6 +128,7 @@ void DataPreprocessing::saveNodeToDatabase(const RoutingNode &node)
 {
     if(_finalDBConnection->saveNode(node) == true)
     {
+        std::cerr << "node saved to finalDB" << std::endl;
     }
     else
     {
@@ -163,6 +140,7 @@ void DataPreprocessing::saveEdgeToDatabase(const RoutingEdge &edge)
 {
     if(_finalDBConnection->saveEdge(edge))
     {
+        std::cerr << "edge saved to finalDB" << std::endl;
     }
     else
     {
@@ -170,7 +148,22 @@ void DataPreprocessing::saveEdgeToDatabase(const RoutingEdge &edge)
     }
 }
 
-//TODO kategorisierungsfunktionen implementieren
+//TODO kategorisierungsfunktion implementieren
+void DataPreprocessing::categorizeEdge(const RoutingEdge &edge)
+{
+    if(edge.hasStairs)
+    {
+    }
+    //...
+    
+    //edge.getSurfaceQuality aufrufen und damit die Qualitaet festlegen
+    
+    //noch zu klaeren, wie es im Detail läuft (wird ein laengerer if-else-zweig)
+}
+
+//int DataPreprocessing::getStreetType();
+//int DataPreprocessing::getStreetSurfaceQuality();
+//int DataPreprocessing::getStreetSurfaceType();
 
 namespace biker_tests
 {    
