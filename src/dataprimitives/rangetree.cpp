a62fd32aedaa2412263fc41b2914728869f69286
Lena brüder (eee) <lena_lebt@gmx.de>
2012-02-22 17:09:54 +0100

    Rangetree funktioniert jetzt. Müssen noch ein paar Speichersparänderungen rein.

--- src/dataprimitives/rangetree.cpp
+++ src/dataprimitives/rangetree.cpp
@@ -1,6 +1,13 @@
 #include "rangetree.hpp"
 
 #include "tests.hpp"
+#include <boost/random/linear_congruential.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/variate_generator.hpp>
+#include <boost/generator_iterator.hpp>
+#include <ctime>
+#include <QSet>
 
 namespace biker_tests
 {
@@ -9,19 +16,19 @@ namespace biker_tests
         RangeTree<int> tree;
         
         CHECK(!tree.contains(5));
-        std::cerr << tree << std::endl;
         tree.insert(5);
+        CHECK(tree.contains(5));
+        tree.insert(5);
+        std::cerr << tree << std::endl;
         CHECK(tree.contains(5));
         CHECK(!tree.contains(6));
         CHECK(!tree.contains(6));
-        std::cerr << tree << std::endl;
         tree.insert(6);
         CHECK(tree.contains(6));
         CHECK(!tree.contains(4));
         CHECK(!tree.contains(3));
         CHECK(!tree.contains(2));
         CHECK(!tree.contains(1));
-        std::cerr << tree << std::endl;
         tree.insert(4);
         CHECK(tree.contains(6));
         CHECK(tree.contains(5));
@@ -29,13 +36,11 @@ namespace biker_tests
         CHECK(!tree.contains(3));
         CHECK(!tree.contains(2));
         CHECK(!tree.contains(1));
-        std::cerr << tree << std::endl;
         tree.insert(1);
         CHECK(tree.contains(4));
         CHECK(!tree.contains(3));
         CHECK(!tree.contains(2));
         CHECK(tree.contains(1));
-        std::cerr << tree << std::endl;
         tree.insert(10);
         CHECK(tree.contains(5));
         CHECK(tree.contains(6));
@@ -45,6 +50,7 @@ namespace biker_tests
         CHECK(tree.contains(1));
         CHECK(tree.contains(10));
         CHECK(!tree.contains(3));
+        tree.insert(5);
         std::cerr << tree << std::endl;
         tree.insert(3);
         CHECK(tree.contains(5));
@@ -53,7 +59,6 @@ namespace biker_tests
         CHECK(tree.contains(1));
         CHECK(tree.contains(10));
         CHECK(tree.contains(3));
-        std::cerr << tree << std::endl;
         tree.insert(2);
         CHECK(tree.contains(5));
         CHECK(tree.contains(6));
@@ -68,8 +73,23 @@ namespace biker_tests
         RangeTree<int> tree2;
         for (int i=0; i<5000000; i++)
             tree2.insert(i);
-        std::cerr << tree2;
+        std::cerr << tree2 << std::endl;
         
+        RangeTree<int> tree3;
+        boost::minstd_rand generator(static_cast<unsigned int>(std::time(0)));
+        typedef boost::variate_generator<boost::minstd_rand&, boost::uniform_int<> > gen_type;
+        gen_type dist(generator, boost::uniform_int<>(1, 20000));
+        
+        QSet<int> set;
+        for (int i=0; i<50000; i++)
+        {
+            int k = dist();
+            tree3.insert(k);
+            set.insert(k);
+        }
+        //std::cerr << tree3 << std::endl;
+        std::cerr << "size for 50000 numbers: " << tree3.sizeInBytes()
+            << " instead of " << set.size()*sizeof(int) << std::endl;
         return EXIT_FAILURE;
     }
 }
--- src/dataprimitives/rangetree.hpp
+++ src/dataprimitives/rangetree.hpp
@@ -15,8 +15,9 @@ private:
     
     int getUBound()
     {
-        if (lBound != 0)
+        if (lBound == 0)
         {
+            assert(rTree != 0);
             return rTree->getUBound();
         }
         else
@@ -26,13 +27,68 @@ private:
     }
     int getLBound()
     {
-        if (lBound != 0)
+        if (lBound == 0)
         {
+            assert(lTree != 0);
             return lTree->getLBound();
         }
         else
         {
             return lBound;
+        }
+    }
+    
+    RangeTree<T>* cutRightmostLeafTreeIfNecessary(const T& otherLBound)
+    {
+        if (this->lBound == 0)
+        {
+            assert(this->rTree != 0);
+            RangeTree<T>* retTree = rTree->cutRightmostLeafTreeIfNecessary(otherLBound);
+            if (retTree == this->rTree)
+            {   //Endknoten gefunden. Ausschneiden und hochpropagieren.
+                RangeTree<T>* tmp = lTree;
+                *this = *lTree;
+                tmp->lTree = 0;
+                tmp->rTree = 0;
+                delete tmp;
+                return retTree;
+            }
+            return retTree;
+        }
+        else
+        {
+            if (otherLBound == uBound + 1)
+                return this;
+            else
+                return 0;
+        }
+    }
+    void insertLeafTree(RangeTree<T>* leafTree)
+    {
+        assert(leafTree->lBound != 0);
+        if (this->lBound == 0)
+        {
+            if (this->uBound > leafTree->uBound)
+            {
+                lTree->insertLeafTree(leafTree);
+            }
+            else
+                std::cerr << "müp" << std::endl;
+        }
+        else
+        {
+            if (this->lBound == leafTree->uBound + 1)
+            {
+                this->lBound = leafTree->lBound;
+            }
+            else if (this->uBound == leafTree->lBound - 1)
+            {
+                this->uBound = leafTree->uBound;
+            }
+            else
+            {
+                std::cerr << "möp" << std::endl;
+            }
         }
     }
     
@@ -42,7 +98,7 @@ private:
         {
             assert(lTree != 0);
             assert(rTree != 0);
-            if (lTree->uBound + 1 == rTree->lBound)
+            if ((lTree->lBound != 0) && (lTree->uBound + 1 == rTree->lBound))
             {
                 lBound = lTree->lBound;
                 uBound = rTree->uBound;
@@ -51,19 +107,51 @@ private:
                 lTree=0;
                 rTree=0;
             }
+            else
+            {
+                RangeTree<T>* rightmost = lTree->cutRightmostLeafTreeIfNecessary(rTree->getLBound());
+                if (rightmost)
+                {
+                    rTree->insertLeafTree(rightmost);
+                    if (rightmost == lTree)
+                    {
+                        //Kopie von rTree machen nach *this
+                        RangeTree<T>* tmp = rTree;
+                        this->lBound = rTree->lBound;
+                        this->uBound = rTree->uBound;
+                        this->lTree = rTree->lTree;
+                        this->rTree = rTree->rTree;
+                        
+                        //sonst gibts Speicherlecks oder tmp löscht mir r-und lTree weg
+                        tmp->lTree=0;
+                        tmp->rTree=0;
+                        delete tmp;
+                    }
+                    else
+                        this->uBound = rightmost->lBound;
+                    delete rightmost;
+                }
+            }
         }
     }
 public:
-    RangeTree(const RangeTree& other)
+    RangeTree(const RangeTree<T>& other)
         : lBound(other.lBound), uBound(other.uBound),
             lTree(other.lTree), rTree(other.rTree)
     {
-        std::cerr << ".";
+        
     }
     RangeTree() :
         lBound(0), uBound(0), lTree(0), rTree(0)
     {
         
+    }
+    ~RangeTree()
+    {
+        if (lTree)
+            delete lTree;
+        if (rTree)
+            delete rTree;
     }
     
     void insert(const T& element)
@@ -82,7 +170,6 @@ public:
         {   //Dies ist ein Mittelknoten, kein Blattknoten.
             assert(lTree != 0);
             assert(rTree != 0);
-            //TODO: Ausgleichen. Dafür: Grenzen prüfen der Kinder... Reicht das?
             if (element < uBound)
             {
                 lTree->insert(element);
@@ -98,16 +185,20 @@ public:
         {
             //Wenn man einfach die Grenzen erweitern kann, weil element direkt daneben liegt: Das tun.
             if (lBound == element + 1)
+            {
                 lBound = element;
+            }
             else if (uBound == element - 1)
+            {
                 uBound = element;
+            }
             else
             {   //Mehr als Grenzen erweitern: Knoten teilen.
                 if (element < lBound)
                 {   //Element links einfügen. Alter Baum wird rechter Teilbaum.
-                    this->rTree = new RangeTree(*this);
+                    this->rTree = new RangeTree<T>(*this);
                     
-                    this->lTree = new RangeTree();
+                    this->lTree = new RangeTree<T>();
                     lTree->lBound = element;
                     lTree->uBound = element;
                     
@@ -116,9 +207,9 @@ public:
                 }
                 else if (uBound < element)
                 {   //Element rechts einfügen. Alter Baum wird linker Teilbaum.
-                    this->lTree = new RangeTree(*this);
+                    this->lTree = new RangeTree<T>(*this);
                     
-                    this->rTree = new RangeTree();
+                    this->rTree = new RangeTree<T>();
                     rTree->lBound = element;
                     rTree->uBound = element;
                     
@@ -149,15 +240,26 @@ public:
             else if (uBound == element)
                 return true;
             else if (element < uBound)
+            {
+                assert(lTree != 0);
                 return lTree->contains(element);
+            }
             else
+            {
+                assert(rTree != 0);
                 return rTree->contains(element);
+            }
         }
     }
     
     void remove(const T& element)
     {
         
+    }
+    
+    size_t sizeInBytes()
+    {
+        return (lTree!=0 ? lTree->sizeInBytes() : 0) + (rTree!=0 ? rTree->sizeInBytes() : 0) + sizeof(RangeTree);
     }
     
     template <typename U>
